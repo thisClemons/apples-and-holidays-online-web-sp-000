0113af59b02de97bfb8203e2aeffcd04c58b4a1a
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-22 21:23:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,16 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each do |season, holidays|
+    puts season.capitalize + ":"
+    holidays.each do |holiday, items|
+      prints holiday.capitalize + ": "
+      puts items.split(", ")
+    end
+
+
+  end
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
