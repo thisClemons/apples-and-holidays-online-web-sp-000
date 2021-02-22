eed16436adfd2f0fefb75886134654053a586de9
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-22 21:45:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -93,7 +93,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  bbqDays = []
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supply|
+      bbqDays.pop(holiday) if supply.include?("BBQ")
+    end
+  end
 end
 
 #binding.pry
