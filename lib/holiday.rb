ec586431230e6000a5de21c036840c7cae302a37
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-22 21:27:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holidays.each do |holiday, items|
       print "#{holiday.capitalize}: "
-      puts items.split(", ")
+      puts items.join(", ")
     end
 
 
